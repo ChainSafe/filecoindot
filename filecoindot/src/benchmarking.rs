053bc61ba944b1433a6ea720e1d8b9ce321de227
willesxm <willeslau@gmail.com>
2021-10-13 18:56:18 +0800

    added benchmarks

--- filecoindot/src/benchmarking.rs
+++ filecoindot/src/benchmarking.rs
@@ -1,11 +1,35 @@
 // Copyright 2021 ChainSafe Systems
 // SPDX-License-Identifier: LGPL-3.0-only
 
-//! Benchmarking setup for pallet-template
+//! Benchmarking setup for filecoindot
 
-use super::*;
+#![cfg(feature = "runtime-benchmarks")]
 
 #[allow(unused)]
-use crate::Module as Template;
-use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
+use crate::*;
+use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite, vec, whitelisted_caller};
 use frame_system::RawOrigin;
+
+benchmarks! {
+    add_relayer {
+        let relayer: T::AccountId = account("relayer", 0, 0);
+    }: _(RawOrigin::Root, relayer)
+
+    remove_relayer {
+        let relayer: T::AccountId = account("relayer", 0, 0);
+        <Call<T>>::add_relayer(RawOrigin::Root, relayer.clone());
+    }: _(RawOrigin::Root, relayer)
+
+    set_vote_threshold {
+        let _ in 0..3 {
+            let relayer: T::AccountId = account("relayer", 0, 0);
+            <Call<T>>::add_relayer(RawOrigin::Root, relayer);
+        }
+    }: _(RawOrigin::Root, 2)
+
+    submit_block_vote {
+        let caller: T::AccountId = whitelisted_caller();
+        <Call<T>>::add_relayer(RawOrigin::Root, caller);
+        <Call<T>>::set_vote_threshold(RawOrigin::Root, 1);
+    }: _(RawOrigin::Root, vec![1,2], vec![2,3])
+}
