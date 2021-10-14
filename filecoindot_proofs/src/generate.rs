bd61ce9d667f5ede59e823c7d4030cef2b7e4540
Matthias Seitz <matthias.seitz@outlook.de>
2021-10-14 18:09:51 +0200

    chore: some proof crafting

--- filecoindot_proofs/src/generate.rs
+++ filecoindot_proofs/src/generate.rs
@@ -4,23 +4,56 @@
 //! Generation of proofs for AMT and HAMT tries
 
 use crate::{Result, TrieError};
-use ipld_amt::Amt;
+
 use ipld_hamt::Hamt;
 
-/// Generate an inclusion proof for the value in the given AMT.
-pub fn generate_amt_proof<'db, V, BS>(amt: &Amt<'db, V, BS>, value: V) -> Result<Vec<Vec<u8>>> {
-    // the stack of nodes representing the path in the trie
-    let mut stack: Vec<u8> = vec![];
+pub mod amt {
+    use crate::{Result, TrieError};
+    use cid::Cid;
+    use ipld_amt::{Amt, Node, Root};
 
-    // The trie nodes comprising the final proof
-    let mut proof_nodes = Vec::new();
+    /// The type to keep track of a node's location in the path
+    struct StackEntry {
+        node_hash: Vec<u8>,
 
-    Ok(proof_nodes)
+        /// Leaf references used when the proof is constructed
+        leafs: Vec<LeafReference>,
+        /// index into the final proof vector for this node
+        output_index: Option<usize>,
+    }
+
+    /// A reference to a leaf node under a certain node
+    enum LeafReference {
+        ///
+        Value(Cid),
+        // TODO this could be a subtree
+        Link(usize),
+    }
+
+    /// Generate an inclusion proof for the value in the given AMT.
+    ///
+    /// A node can either be a leaf node that contains the values or a link node that contains an array of other Cid or cached sub nodes
+    pub fn generate_proof<'bs, V, BS>(node: &Amt<'bs, V, BS>, value: V) -> Result<Vec<Vec<u8>>> {
+        // the stack of nodes representing the path in the trie where each entry is a child node of the preceding entry
+        let mut stack: Vec<StackEntry> = vec![];
+
+        // The trie nodes comprising the final proof
+        let mut proof_nodes = Vec::new();
+
+        // loop over the all nodes of the tree until we've found the leaf value and store
+
+        Ok(proof_nodes)
+    }
 }
 
-/// Generate an inclusion proof for the value in the given HAMT.
-pub fn generate_hamt_proof<'db, V, BS>(amt: &Hamt<'db, BS, V>, value: V) -> Result<Vec<Vec<u8>>> {
-    // each node can have up to 32 children
+pub mod hamt {
+    use crate::{Result, TrieError};
+    use ipld_hamt::{Hamt, Node};
 
-    todo!()
+    /// Generate an inclusion proof for the value in the given HAMT.
+    pub fn generate_proof<'db, V, BS>(hamt: &Hamt<'db, BS, V>, value: V) -> Result<Vec<Vec<u8>>> {
+        // each node can have up to 32 children
+
+        todo!()
+    }
 }
