5704fcbd8314d3a5a323af791ec6b50c5c8a4a38
Jovansonlee Cesar <ivanceras@gmail.com>
2021-11-17 20:49:08 +0800

    Refactor block submission (#81)
    * refactor(types)!: move the `voted` field from `BlockSubmissionProposal` struct
    into a new storage type called `BlockProposalVotes`.
    
    * chore(ocw): expose the `BlockSubmissionProposal::is_expired` method as `pub` instead of `pub(crate)` for consistency with other methods
    
    * chor(ocw): remove unused imports
    
    * refactor(types): move `BlockSubmissionProposal::resolve` to pallet's
    runtime `resolve_proposal` to prevent `MessageRootCidCounter` leaking
    into `BlockSubmissionProposal` struct

--- filecoindot/src/types.rs
+++ filecoindot/src/types.rs
@@ -3,17 +3,14 @@
 
 use frame_support::pallet_prelude::*;
 use frame_support::sp_std;
-use frame_support::sp_std::collections::btree_set::BTreeSet;
 use frame_system::{Origin, RawOrigin};
 
-use crate::{pallet, Config, Error, MessageRootCidCounter, Relayers};
+use crate::{Config, Relayers};
 
 /// The filecoin block submission proposal
 #[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug)]
 pub(crate) struct BlockSubmissionProposal<T: Config> {
     proposer: T::AccountId,
-    /// The accounts with voted
-    voted: BTreeSet<T::AccountId>,
     /// The status of the proposal
     status: ProposalStatus,
     /// The block number that the proposal started
@@ -30,7 +27,7 @@ impl<T: Config> BlockSubmissionProposal<T> {
     ) -> Self {
         BlockSubmissionProposal {
             proposer,
-            voted: BTreeSet::new(),
+            //voted: BTreeSet::new(),
             status: ProposalStatus::Active,
             start_block,
             end_block,
@@ -42,77 +39,12 @@ impl<T: Config> BlockSubmissionProposal<T> {
         &self.status
     }
 
-    /// Vote for the proposal. Will reject the operation if its status is invalid
-    /// The content of the vote is actually the message root of the block
-    pub fn vote(
-        &mut self,
-        block_cid: pallet::BlockCid,
-        message_root: pallet::MessageRootCid,
-        who: T::AccountId,
-        when: &T::BlockNumber,
-        threshold: u32,
-    ) -> Result<(), Error<T>> {
-        ensure!(!self.is_voted(&who), Error::<T>::AlreadyVoted);
-        ensure!(
-            self.status == ProposalStatus::Active,
-            Error::<T>::ProposalCompleted
-        );
-
-        // when expired, we set the status to be rejected
-        if self.is_expired(when) {
-            self.status = ProposalStatus::Rejected;
-            return Err(Error::<T>::ProposalExpired);
-        }
-
-        // MessageRootCidCounter leaked into the struct, well not the best way for encapsulation
-        // but works for now, come back later to fix this.
-        let count = 1 + MessageRootCidCounter::<T>::get(&block_cid, &message_root).unwrap_or(0);
-        if count >= threshold {
-            self.status = ProposalStatus::Approved;
-        }
-
-        MessageRootCidCounter::<T>::insert(&block_cid, &message_root, count);
-        self.voted.insert(who);
-
-        Ok(())
-    }
-
-    /// Resolve the proposal status
-    pub fn resolve(
-        &mut self,
-        block_cid: &[u8],
-        when: &T::BlockNumber,
-        threshold: u32,
-    ) -> Result<(), Error<T>> {
-        ensure!(
-            self.status == ProposalStatus::Active,
-            Error::<T>::ProposalCompleted
-        );
-
-        // when expired, we set the status to be rejected
-        if self.is_expired(when) {
-            self.status = ProposalStatus::Rejected;
-        } else {
-            // MessageRootCidCounter leaked into the struct, well not the best way for encapsulation
-            // but works for now, come back later to fix this.
-            for (_, count) in MessageRootCidCounter::<T>::iter_prefix(block_cid) {
-                if count >= threshold {
-                    self.status = ProposalStatus::Approved;
-                    break;
-                }
-            }
-        }
-
-        Ok(())
-    }
-
-    /// Checks if the account has already voted
-    fn is_voted(&self, who: &T::AccountId) -> bool {
-        self.voted.contains(who)
+    pub fn set_status(&mut self, new_status: ProposalStatus) {
+        self.status = new_status;
     }
 
     /// Whether the proposal is still active, i.e. can vote
-    fn is_expired(&self, now: &T::BlockNumber) -> bool {
+    pub fn is_expired(&self, now: &T::BlockNumber) -> bool {
         now.gt(&self.end_block)
     }
 }
