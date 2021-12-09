c3dfc20302253ad0ae13fdb598f9067797456d53
Bernard <bero4net@gmail.com>
2021-12-09 13:22:34 +0100

    implement header

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,19 @@ import {BrowserRouter} from "react-router-dom";
 import { Provider } from 'react-redux';
 import {store} from "./ducks/store";
 import {Home} from "./pages/Home";
+import {Settings} from "./containers/Settings";
+import {Header} from "./containers/Header";
+import Container from '@mui/material/Container';
 
 function App() {
   return (
       <BrowserRouter>
         <Provider store={store}>
-            <Home />
+            <Header />
+            <Container fixed>
+                <Home />
+            </Container>
+            <Settings />
         </Provider>
       </BrowserRouter>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {CssBaseline} from "@mui/material";
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
