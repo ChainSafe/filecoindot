4f18c69e64c0d29c1448561e2d1dbf4b1d194f95
clearloop <mercuryfletcher@gmail.com>
2021-12-13 15:24:45 +0800

    chore(node): add runtime interfaces to node

--- substrate-node-example/node/src/service.rs
+++ substrate-node-example/node/src/service.rs
@@ -21,10 +21,15 @@ pub struct ExecutorDispatch;
 impl sc_executor::NativeExecutionDispatch for ExecutorDispatch {
     /// Only enable the benchmarking host functions when we actually want to benchmark.
     #[cfg(feature = "runtime-benchmarks")]
-    type ExtendHostFunctions = frame_benchmarking::benchmarking::HostFunctions;
+    type ExtendHostFunctions = (
+        frame_benchmarking::benchmarking::HostFunctions,
+        filecoindot_io::forest_proof_verify::HostFunctions,
+    );
     /// Otherwise we only use the default Substrate host functions.
     #[cfg(not(feature = "runtime-benchmarks"))]
-    type ExtendHostFunctions = ();
+    type ExtendHostFunctions = (
+        filecoindot_io::forest_proof_verify::HostFunctions,
+    );
 
     fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
         node_template_runtime::api::dispatch(method, data)
