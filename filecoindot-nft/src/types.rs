11eaf368e7168c30449f2de8b33ea7fa794cddeb
willeslau <willeslau@gmail.com>
2021-12-15 15:09:26 +0800

    nft implementation (#121)
    * initial nft implementation
    
    * add proof to nft
    
    * add more comments and readme
    
    * modify docs
    
    * cargo fmt

---
+++ filecoindot-nft/src/types.rs
@@ -0,0 +1,35 @@
+// Copyright 2021 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
+use frame_support::pallet_prelude::*;
+use frame_support::sp_std;
+use scale_info::TypeInfo;
+use serde::{Deserialize, Serialize};
+use sp_std::prelude::*;
+
+#[derive(Encode, Decode, Clone, RuntimeDebug, PartialEq, Eq, TypeInfo, Default)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub struct ClassData {
+    /// The data stored in the class
+    pub data: Vec<u8>,
+}
+
+impl ClassData {
+    pub fn new(data: Vec<u8>) -> Self {
+        ClassData { data }
+    }
+}
+
+#[derive(Encode, Decode, Clone, RuntimeDebug, PartialEq, Eq, TypeInfo)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub struct TokenData {
+    /// The cid of the data in filecoin
+    pub cid: Vec<u8>,
+    pub proof: Vec<Vec<u8>>,
+}
+
+impl TokenData {
+    pub fn new(cid: Vec<u8>, proof: Vec<Vec<u8>>) -> Self {
+        TokenData { cid, proof }
+    }
+}
